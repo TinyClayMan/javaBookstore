f919b1c0757cf3b87ea203c887d2749da967a614
TinyClayMan <tinyclayman@gmail.com>
2021-04-30 02:08:00 +0300

    Initial commit

---
+++ src/test/java/com/tinyclayman/javabookstore/JavaBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyclayman.javabookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
